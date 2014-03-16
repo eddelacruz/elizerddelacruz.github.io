b799b330107c50d9100ac5db9fceed4a952d55b5
eddelacruz <eddelacruz@dynamicobjx.com>
2014-03-16 19:36:49 +0800

    stop()

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             $(ghost).stop().animate({'backgroundColor': '#000'}, 400);
 
             var menuCaption = $(this).find('.menu-caption');
-            $(menuCaption).fadeOut(400);
+            $(menuCaption).stop().fadeOut(400);
 
             var menuTitle = $(this).find('h1');
             $(menuTitle).stop().animate({'color': '#FFF'}, 400);
@@ -15,7 +15,7 @@ $(document).ready(function() {
             $(ghost).stop().animate({'backgroundColor': '#FFF'}, 400);
 
             var menuCaption = $(this).find('.menu-caption');
-            $(menuCaption).fadeIn(400);
+            $(menuCaption).stop().fadeIn(400);
 
             var menuTitle = $(this).find('h1');
             $(menuTitle).stop().animate({'color': '#000'}, 400);
