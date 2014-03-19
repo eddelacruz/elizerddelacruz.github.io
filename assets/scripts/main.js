81f3bbb61c77c72e3edf565c2c04ed2f36cb9b50
eddelacruz <eddelacruz@dynamicobjx.com>
2014-03-20 01:34:15 +0800

    gradient

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
             var menuTitle = $(this).find('h1');
             $(menuTitle).stop().animate({'color': '#FFF'}, 400);
       }); 
+
       $('.menu > li').mouseenter(function(){
             var ghost = $(this).find('.ghost-title');
             $(ghost).stop().animate({'backgroundColor': '#FFF'}, 400);
@@ -21,4 +22,47 @@ $(document).ready(function() {
             var menuTitle = $(this).find('h1');
             $(menuTitle).stop().animate({'color': '#000'}, 400);
       });
+
+      //looping of background images
+
+      // var currentBackground = 1;
+      // var backgrounds = [];
+      // backgrounds[0] = "../images/stock/back1.jpg";
+      // backgrounds[1] = "../images/stock/back2.jpg";
+      // backgrounds[2] = "../images/stock/back3.jpg";
+      // backgrounds[3] = "../images/stock/back6.jpg";
+
+      // $('.background-image').fadeOut(100, function() {
+      //       $(this).css({
+      //             // 'background-image' : "url('" + backgrounds[currentBackground] + "')"
+      //             // 'background-image' : url('../images/stock/back1.jpg')
+      //       });
+      //       $(this).fadeIn(100);
+      // });
+      setTimeout(changeBackground, 10000);
+});
+
+var currentBackground = 0;
+var backgrounds = [];
+backgrounds[0] = '../elizerddelacruz.github.io/assets/images/stock/back6.jpg';
+backgrounds[1] = '../elizerddelacruz.github.io/assets/images/stock/back5.jpg';
+backgrounds[2] = '../elizerddelacruz.github.io/assets/images/stock/back4.jpg';
+
+function changeBackground() {
+    currentBackground++;
+    if(currentBackground > 2) currentBackground = 0;
+
+    $('.background-image').hide(0,function() {
+        $('.background-image').css({
+            'background-image' : "url('" + backgrounds[currentBackground] + "')"
+        });
+        $('.background-image').show();
+    });
+
+
+    setTimeout(changeBackground, 10000);
+}
+
+$(document).ready(function() {
+            
 });
