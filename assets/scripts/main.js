0f29f05732d1c4ab4bce0925e91d4d2baca4654f
eddelacruz <eddelacruz@dynamicobjx.com>
2014-03-20 00:04:50 +0800

    works

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-      $('.menu-item').mouseleave(function(){
+      $('.menu > li').mouseleave(function(){
             var ghost = $(this).find('.ghost-title');
-            $(ghost).stop().animate({'backgroundColor': '#000'}, 400);
+            // $(ghost).stop().animate({'backgroundColor': '#000'}, 400);
+            $(ghost).stop().animate({'backgroundColor': '#ff0062'}, 400);
 
             var menuCaption = $(this).find('.menu-caption');
             $(menuCaption).stop().fadeOut(400);
@@ -10,7 +11,7 @@ $(document).ready(function() {
             var menuTitle = $(this).find('h1');
             $(menuTitle).stop().animate({'color': '#FFF'}, 400);
       }); 
-      $('.menu-item').mouseenter(function(){
+      $('.menu > li').mouseenter(function(){
             var ghost = $(this).find('.ghost-title');
             $(ghost).stop().animate({'backgroundColor': '#FFF'}, 400);
 
